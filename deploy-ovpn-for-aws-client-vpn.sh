0d81786f20058bac8451744596478fc47a2ca4df
enpipi <big00eye@gmail.com>
2021-08-20 01:33:51 +0900

    Update deploy-ovpn-for-aws-client-vpn.sh

--- deploy-ovpn-for-aws-client-vpn.sh
+++ deploy-ovpn-for-aws-client-vpn.sh
@@ -23,7 +23,7 @@
 #######################################
 
 # TODO(enpipi) : Checking the behavior when using Active Directory authentication. (enhancement #1)
-VERSION='0.1.0'
+VERSION='0.2.0'
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
 # Output info log with timestamp
